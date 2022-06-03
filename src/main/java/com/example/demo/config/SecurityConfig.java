bd5be3749242bfa4ad038f9ae1f7ff6265c74850
UtsavDabhi16 <utsav.inexture@gmail.com>
2022-06-03 18:46:50 +0530

    03/06/2022

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("ALL")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/home");
+
+
+    }
+}
